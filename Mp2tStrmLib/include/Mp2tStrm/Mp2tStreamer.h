0446978f6a284bad95d59acb30f3498f30f6c80e
James McAvoy <jimcavoy@thetastream.com>
2024-08-30 15:01:02 -0400

    Add new Metric functions (#13)
    Add new member functions to query runtime metrics.

--- Mp2tStrmLib/include/Mp2tStrm/Mp2tStreamer.h
+++ Mp2tStrmLib/include/Mp2tStrm/Mp2tStreamer.h
@@ -7,44 +7,50 @@
 
 namespace ThetaStream
 {
-	class Mp2tStreamer
-	{
-	public:
-		Mp2tStreamer();
-		Mp2tStreamer(const ThetaStream::CommandLineParser& arguments);
-		~Mp2tStreamer();
+    class Mp2tStreamer
+    {
+    public:
+        Mp2tStreamer();
+        Mp2tStreamer(const ThetaStream::CommandLineParser& arguments);
+        ~Mp2tStreamer();
 
-		void init(const ThetaStream::CommandLineParser& arguments);
+        void init(const ThetaStream::CommandLineParser& arguments);
 
-		void probe();
+        void probe();
 
-		int run();
+        int run();
 
-		void stop();
+        void stop();
 
-		void pause();
+        void pause();
 
-		uint64_t tsPacketsRead() const;
+        uint64_t tsPacketsRead() const;
 
-		uint64_t udpPacketsSent() const;
+        uint64_t udpPacketsSent() const;
 
-		double duration() const;
+        double duration() const;
 
-		double averageBitrate() const;
+        double averageBitrate() const;
 
-		std::string metadataCarriage() const;
+        std::string metadataCarriage() const;
 
-		int metadataFrequency() const;
+        int metadataFrequency() const;
 
-		double framesPerSecond() const;
+        double framesPerSecond() const;
 
-		int width() const;
+        int width() const;
 
-		int height() const;
+        int height() const;
 
-	private:
-		class Impl;
-		std::unique_ptr<Impl> _pimpl;
-	};
+        int bytesSent() const;
+
+        long position() const;
+
+        int framerate() const;
+
+    private:
+        class Impl;
+        std::unique_ptr<Impl> _pimpl;
+    };
 }
 
